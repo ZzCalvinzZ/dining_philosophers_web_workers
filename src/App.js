73d707728787964a0e7d001dbdd3c390575da2f9
Calvin Collins <calvinc@surveymonkey.com>
2019-04-28 12:32:12 -0400

    workers working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Table from "./Components/Table/";
 import Fork from "./Components/Fork/";
 import Philosopher from "./Components/Philosopher/";
+import philosopherWorker from "./philosopherWorker";
+import WebWorker from "./WebWorker";
 import "./App.css";
 
 class App extends Component {
@@ -19,13 +21,42 @@ class App extends Component {
       Philosopher,
       Philosopher,
       Philosopher
-    ]
+    ],
+    speed: 10,
+    solution: "deadlock"
   };
 
   render() {
-    const { forks, philosophers } = this.state;
+    const { forks, philosophers, solution, speed } = this.state;
+    const colors = ["red", "blue", "green", "yellow", "purple"];
+    const workers = philosophers.map(p => {
+      return new WebWorker(philosopherWorker);
+    });
+
+    workers[0].addEventListener("message", e => console.log(e.data), false);
+    workers[0].postMessage("eat");
+
+    // if (solution === "deadlock") {
+    // }
+
+    // if (solution === "no-deadlock") {
+    // }
+
     return (
       <div className="app">
+        <button
+          className="deadlock-btn"
+          onClick={() => this.setState({ solution: "deadlock" })}
+        >
+          Deadlock Solution
+        </button>
+        <button
+          className="no-deadlock-btn"
+          onClick={() => this.setState({ solution: "no-deadlock" })}
+        >
+          No Deadlock Solution
+        </button>
+        <div>{solution}</div>
         <Table forks={forks} philosophers={philosophers} />
       </div>
     );
---
+++ src/WebWorker.js
@@ -0,0 +1,11 @@
+// WebWorker.js
+
+export default class WebWorker {
+  constructor(worker) {
+    let code = worker.toString();
+    code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
+
+    const blob = new Blob([code], { type: "application/javascript" });
+    return new Worker(URL.createObjectURL(blob));
+  }
+}
